bf1d985f453cf83e87fb8af7807a59d9ee3ac295
SHAAPERASOPHIA <sophieshaapera@gmail.com>
2024-08-26 03:25:15 -0700

    bank code
    code helps to create a bank code

---
+++ token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract XofeyaToken is ERC20, Ownable (msg.sender) {
+    constructor() ERC20("XofeyaToken", "XTN") {
+        _mint(msg.sender, 30 * 10 ** 18);
+    }
+
+    function mint(address, uint256 amount) public onlyOwner{ 
+             uint256 amountWithDecimals = amount * 10 ** 18;
+           _mint(0x025a67f82571889BE513F7D8581fA206DE5e47AC, amountWithDecimals) ;
+    }
+}
